d56a31a3f5c3d029f9b77583584344568e7384d9
Filipe <filipe.alvescavalcante@gmail.com>
2025-11-19 21:06:12 -0300

    feat: :art: pivot table working

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,14 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Employer extends Model
+class Tag extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
-
     public function jobs()
     {
-        return $this->hasMany(Job::class);
+        return $this->belongsToMany(Job::class, 'job_tag', 'tag_id', 'job_listing_id');
     }
 }
