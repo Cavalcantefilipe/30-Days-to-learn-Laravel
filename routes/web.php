edde7d47a9821d89a7de7cecebaf5ef94b63a9f4
Filipe <filipe.alvescavalcante@gmail.com>
2025-11-28 17:03:11 -0300

    feat: Controller and day 19

--- routes/web.php
+++ routes/web.php
@@ -1,81 +1,18 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::view('/', 'home');
+
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
 });
 
-Route::get('/jobs', function () {
-
-    $jobs = Job::with(['employer'])->latest()->simplePaginate(10);
-
-    return view('jobs.index', [
-        'jobs' => $jobs,
-    ]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-Route::post('jobs', function () {
-
-    request()->validate([
-        'title' => 'required|string|min:3',
-        'salary' => 'required|string|max:255',
-    ]);
-
-    $job = Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect("/jobs");
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-
-    request()->validate([
-        'title' => 'required|string|min:3',
-        'salary' => 'required|string|max:255',
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-
-    Job::findOrFail($id)->delete();
-
-    return redirect("/jobs");
-});
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', [
-        'job' => $job,
-    ]);
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.edit', [
-        'job' => $job,
-    ]);
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
