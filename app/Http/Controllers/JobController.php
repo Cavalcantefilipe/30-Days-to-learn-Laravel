78546f998ca3f6f1ff4ba154b13f14f70b64f5f0
Filipe <filipe.alvescavalcante@gmail.com>
2025-12-04 16:29:11 -0300

    feat: day 24 email

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -43,6 +45,8 @@ class JobController extends Controller
             'salary' => $request->input('salary'),
             'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user->email)->send(new JobPosted($job));
 
         return redirect("/jobs");
     }
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
 
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/jobs/create', [JobController::class, 'create']);
